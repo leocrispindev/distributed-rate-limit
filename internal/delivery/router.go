1e142682c039129f038485d1811cfc78605637a7
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2025-08-26 22:43:45 -0300

    create bucket route

--- internal/delivery/router.go
+++ internal/delivery/router.go
@@ -1,16 +1,30 @@
 package delivery
 
 import (
+	"concurrency-hazelcast/internal/delivery/middleware"
 	"concurrency-hazelcast/internal/handler"
+	"concurrency-hazelcast/internal/infrastructure/repository/hazelcast"
+	createtokenbucket "concurrency-hazelcast/internal/usecase/bucket/createTokenBucket"
+	"concurrency-hazelcast/internal/usecase/ratelimit"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
 
-	handler := handler.NewHandler()
+	hzClient, _ := hazelcast.NewHazelcastClient()
+	NoSQLRepository := hazelcast.NewHazelcastRepository(hzClient)
 
-	// Define your routes here
-	router.GET("/example", handler.HelloHandler)
+	// Create Token Bucket
+	createUseCase := createtokenbucket.NewCreateTokenBucketUseCase(NoSQLRepository)
+	createBucketHandler := handler.NewCreateBucketHandler(createUseCase)
+
+	//Middleware
+	rateLimitUseCase := ratelimit.NewRateLimitUseCase(NoSQLRepository)
+
+	authorizationMiddleware := middleware.NewAuthorizationMiddleware(rateLimitUseCase)
+
+	router.GET("/example", authorizationMiddleware.Middleware(), handler.HelloHandler)
+	router.POST("/bucket", createBucketHandler.Handler)
 
 }
